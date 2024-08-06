931198a7f4e62f903b5287b8fda89db48c8d4529
Balkriishna-1195 <venatibalakrishna1195@gmail.com>
2024-08-06 16:12:29 +0530

    adding basic python concepts

---
+++ 01_variables.py
@@ -0,0 +1,91 @@
+# This is a single line comment
+
+"""
+this is a multi-line comment
+"""
+
+# Define a variable
+a = 42 # This is a integer variable
+
+# Print the value that is stored inside the variable
+print(a)
+
+# Float value
+b = 42.345
+print(b)
+
+# Boolean
+c = True # true
+
+# Strings
+d = 'This is a string'
+e = "This is also a string"
+f = """
+This is a multi line string
+"""
+
+# Today's weather is nice
+d = 'Today\'s weather is nice'
+print(d)
+
+d = "Today's weather is nice"
+print(d)
+
+# This is a list
+test_list = ["hello", "world", "python"]
+print(test_list)
+
+# Tuple
+test_tuple = ("hello", "world", "python")
+print(test_tuple)
+
+# Dict
+test_dict = {'a': 1, 'b': 2}
+print(test_dict)
+
+# Set
+# consider the values in an arbitrary way
+test_set = {'a', 'b', "abc"}
+print(test_set)
+
+# type() function -> prints the datatype of the variable
+print(type(test_dict))
+print(type(print))
+
+# Operations
+# Add
+# Sub
+# Multi
+# Divide
+# Integer division
+# Modulo division
+
+# Add
+a = 42
+b = 45.32
+c = a + b
+print(c)
+
+d = a - b
+print(d)
+
+e = a * b
+print(e)
+
+f = 12
+g = 3
+h = f / g
+print(h, type(h))
+
+# Integer division
+h = f // g
+print(h)
+
+# Modulo division
+i = f % g
+print(i)
+
+# Addition
+a = "42"
+b = "43"
+print(a + b) # Concatenation
